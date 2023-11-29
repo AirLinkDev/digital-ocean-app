a0244b732c027976816c8da92f80f745c91061c9
AirLinkDev <thomas.trebacz@gmail.com>
2023-11-28 17:19:51 -0800

    UI update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
 
-import Form from "./Form";
-import Login from "./Login";
+//import Form from "./Form";
+//import Login from "./Login";
 
 import React,{useState} from "react";
 function App() {
-const [getUserText,setUserText] = useState("");
-const [headingText,setHeadingText] = useState("Hello");
+//const [getUserText,setUserText] = useState("");
+//const [headingText,setHeadingText] = useState("Hello");
 const [getColor,setColor] = useState("white");
 
-function handleInput(event){
-  console.log("User entered"+event.target.value);
-  setUserText(event.target.value);
+const [getFirstName,setFirstName]=useState("");
+const [getLastName,setLastName]=useState("");
+function handleFirstname(event){
+  setFirstName(event.target.value);
 }
-function handleClick(){
-  setHeadingText("Hello "+getUserText);
+function handleLastname(event){
+  setLastName(event.target.value);
 }
 function handleHover(){
   setColor("black");
@@ -23,9 +24,10 @@ function handleMouseOut(){
 }
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" onChange={handleInput} placeholder="What's your name?" />
-      <button style={{backgroundColor : getColor}} onClick={handleClick} onMouseOver={handleHover} onMouseOut={handleMouseOut}>Submit</button>
+      <h1>{"Hello "+getFirstName+" "+getLastName}</h1>
+      <input name="fName" onChange={handleFirstname} placeholder="First Name" />
+      <input name="lName" onChange={handleLastname} placeholder="Last Name" />
+      <button style={{backgroundColor : getColor}} onMouseOver={handleHover} onMouseOut={handleMouseOut}>Submit</button>
     </div>
   );
 }
